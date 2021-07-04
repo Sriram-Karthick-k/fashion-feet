719d38ce4b442a49c358ba5be9aa949873e54d7e
Sriram-Karthick-k <kubhair.kk@gmail.com>
2021-07-04 20:50:59 +0530

    after completing side bar on july 4 2021

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,5 @@
-function navbarOpen(){
-  var menuContainer=document.querySelector(".side-bar-container")
-  menuContainer.style.zIndex="1"
-  var sideBar=document.querySelector(".side-bar")
-  sideBar.style.transform="translate(0%)"
-}
-function navbarClose(){
-  var menuContainer=document.querySelector(".side-bar-container")
-  menuContainer.style.zIndex="-1000"
-  var sideBar=document.querySelector(".side-bar")
-  sideBar.style.transform="translate(100%)"
-
-}
+$(document).ready(function () {
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active-sidebar icon-hide');
+  });
+});
